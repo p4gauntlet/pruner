3e753de0b34512cdd2d03a34310e4feec3bcddad
robotrobo <aa8143@nyu.edu>
2020-10-03 15:12:44 +0530

    Remove assignments (#2)
    * adds a pruner which removes all assignment statements in the corresponding program
    
    * moves the visitor code to a new file

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 #include "ir/ir.h"
 #include "toz3Options.h"
+#include "pruner.h"
+
 
 
 int main(int argc, char *const argv[]) {
@@ -12,7 +14,7 @@ int main(int argc, char *const argv[]) {
     AutoCompileContext autoP4toZ3Context(new P4TOZ3::P4toZ3Context);
     auto &options = P4TOZ3::P4toZ3Context::get().options();
     options.langVersion = CompilerOptions::FrontendVersion::P4_16;
-
+    
     if (options.process(argc, argv) != nullptr) {
         options.setInputFile();
     }
@@ -31,8 +33,21 @@ int main(int argc, char *const argv[]) {
 
     if (program != nullptr && ::errorCount() == 0) {
 
-                P4::ToP4 *top4 = new P4::ToP4(&std::cout, false);
-                program->apply(*top4);
+
+                // P4::ToP4 *before = new P4::ToP4(&std::cout, false);
+                // program->apply(*before);
+
+
+                PRUNER::Pruner *pruner = new PRUNER::Pruner();
+                program = program->apply(*pruner);
+
+
+                P4::ToP4 *after = new P4::ToP4(&std::cout, false);
+    
+
+                program->apply(*after);
+
+
 
         }
     
