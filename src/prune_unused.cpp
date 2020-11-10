a149ae1970b9238336bf4f33fff3a561b26b7b55
Anish <aa8143@nyu.edu>
2020-11-10 15:30:28 +0530

    Removing overrides for unchanged functions

--- src/prune_unused.cpp
+++ src/prune_unused.cpp
@@ -1,112 +1,8 @@
 #include "prune_unused.h"
 #include "frontends/p4/sideEffects.h"
 #include "ir/visitor.h"
+#include "pruner_util.h"
 namespace P4PRUNER {
-
-Visitor::profile_t PruneUnused::init_apply(const IR::Node *node) {
-    INFO("Reference map " << refMap);
-    return Transform::init_apply(node);
-}
-
-const IR::Node *PruneUnused::preorder(IR::Type_Error *type) {
-    prune();
-    if (!refMap->isUsed(getOriginal<IR::Type_Error>())) {
-        INFO("Removing " << type);
-        return nullptr;
-    }
-    return type;
-}
-// FIXME
-/* This doesnt seem to work right now
-const IR::Node *PruneUnused::preorder(IR::Declaration_MatchKind *decl) {
-
-    prune();
-    if (!refMap->isUsed(getOriginal<IR::Declaration_MatchKind>())) {
-        INFO("Removing " << type);
-        return nullptr;
-    }
-    return type;
-}
-const IR::Node *PruneUnused::preorder(IR::Type_StructLike *type) {
-
-    prune();
-    if (!refMap->isUsed(getOriginal<IR::Type_StructLike>())) {
-        INFO("Removing " << type);
-        return nullptr;
-    }
-    return type;
-}
-const IR::Node *PruneUnused::preorder(IR::Type_Extern *type) {
-    prune();
-    if (!refMap->isUsed(getOriginal<IR::Type_Extern>())) {
-        INFO("Removing " << type);
-        return nullptr;
-    }
-    return type;
-}
-const IR::Node *PruneUnused::preorder(IR::Type_Method *type) {
-
-    prune();
-    if (!refMap->isUsed(getOriginal<IR::Type_Method>())) {
-        INFO("Removing " << type);
-        return nullptr;
-    }
-    return type;
-}
-
-const IR::Node *PruneUnused::preorder(IR::Type_Enum *type) {
-    prune(); // never remove individual enum members
-    if (!refMap->isUsed(getOriginal<IR::Type_Enum>())) {
-        INFO("Removing " << type);
-        return nullptr;
-    }
-    return type;
-}
-
-const IR::Node *PruneUnused::preorder(IR::Type_SerEnum *type) {
-    prune(); // never remove individual enum members
-    if (!refMap->isUsed(getOriginal<IR::Type_SerEnum>())) {
-        INFO("Removing " << type);
-        return nullptr;
-    }
-    return type;
-}
-*/
-
-const IR::Node *PruneUnused::preorder(IR::P4Control *cont) {
-    if (!refMap->isUsed(getOriginal<IR::IDeclaration>())) {
-        INFO("Removing " << cont);
-        prune();
-        return nullptr;
-    }
-
-    visit(cont->controlLocals, "controlLocals");
-    visit(cont->body);
-    prune();
-    return cont;
-}
-
-const IR::Node *PruneUnused::preorder(IR::P4Parser *cont) {
-    if (!refMap->isUsed(getOriginal<IR::IDeclaration>())) {
-        INFO("Removing " << cont);
-        prune();
-        return nullptr;
-    }
-
-    visit(cont->parserLocals, "parserLocals");
-    visit(cont->states, "states");
-    prune();
-    return cont;
-}
-
-const IR::Node *PruneUnused::preorder(IR::P4Table *table) {
-    if (!refMap->isUsed(getOriginal<IR::IDeclaration>())) {
-        INFO("Removing " << table);
-        table = nullptr;
-    }
-    prune();
-    return table;
-}
 
 const IR::Node *PruneUnused::preorder(IR::Declaration_Variable *decl) {
     prune();
@@ -117,24 +13,30 @@ const IR::Node *PruneUnused::preorder(IR::Declaration_Variable *decl) {
     return decl;
 }
 
-const IR::Node *PruneUnused::process(const IR::IDeclaration *decl) {
-    INFO("Visiting " << decl);
-    if (decl->getName().name == IR::ParserState::verify &&
-        getParent<IR::P4Program>())
-        return decl->getNode();
-    if (refMap->isUsed(getOriginal<IR::IDeclaration>()))
-        return decl->getNode();
-    INFO("Removing " << getOriginal());
-    prune(); // no need to go deeper
-    return nullptr;
-}
-
 const IR::Node *PruneUnused::preorder(IR::Declaration_Instance *decl) {
     // Don't delete instances; they may have consequences on the control-plane
     // API
     // if (decl->getName().name == IR::P4Program::main &&
     //     getParent<IR::P4Program>())
     //     return decl;
+
+    const IR::Node *preorder(IR::Type_Error * type) {
+        // prune();
+        return process(type);
+    }
+    const IR::Node *preorder(IR::Type_StructLike * type) {
+        // prune();
+        return process(type);
+    }
+    const IR::Node *preorder(IR::Type_Extern * type) {
+        // prune();
+        return process(type);
+    }
+    const IR::Node *preorder(IR::Type_Method * type) {
+        // prune();
+        return process(type);
+    }
+
     if (!refMap->isUsed(getOriginal<IR::Declaration_Instance>())) {
 
         // We won't delete extern instances; these may be useful even if not
@@ -160,19 +62,6 @@ const IR::Node *PruneUnused::preorder(IR::Declaration_Instance *decl) {
     // to delete virtual methods
 
     return process(decl);
-}
-
-const IR::Node *PruneUnused::preorder(IR::ParserState *state) {
-    if (state->name == IR::ParserState::accept ||
-        state->name == IR::ParserState::reject ||
-        state->name == IR::ParserState::start)
-        return state;
-
-    if (refMap->isUsed(getOriginal<IR::ParserState>()))
-        return state;
-    INFO("Removing " << state);
-    prune();
-    return nullptr;
 }
 
 } // namespace P4PRUNER
--- src/prune_unused.h
+++ src/prune_unused.h
@@ -8,53 +8,29 @@
 namespace P4PRUNER {
 
 class PruneUnused : public P4::RemoveUnusedDeclarations {
+    const P4::ReferenceMap *refMap;
 
+    // bool giveWarning(const IR::Node *node);
     const IR::Node *process(const IR::IDeclaration *decl);
 
  public:
-    explicit PruneUnused(const P4::ReferenceMap *_refMap) {
-        refMap = _refMap;
-        // setName("PruneUnused");
+    explicit PruneUnused(const P4::ReferenceMap *refMap)
+        : P4::RemoveUnusedDeclarations(refMap) {
+        CHECK_NULL(refMap);
+        setName("PruneUnused");
     }
-
-    Visitor::profile_t init_apply(const IR::Node *root) override;
-
-    const IR::Node *preorder(IR::P4Control *cont) override;
-    const IR::Node *preorder(IR::P4Parser *cont) override;
-    const IR::Node *preorder(IR::P4Table *cont) override;
-    const IR::Node *preorder(IR::ParserState *state) override;
-    // const IR::Node *preorder(IR::Type_Enum *type) override;
-    // const IR::Node *preorder(IR::Type_SerEnum *type) override;
-
-    const IR::Node *preorder(IR::Declaration_Instance *decl) override;
 
     const IR::Node *preorder(IR::Type_Error *type) override;
-    // FIXME
-    // makeshift solution for right now /////////////////////////////////
-    // const IR::Node *preorder(IR::Type_StructLike *type);
-    // const IR::Node *preorder(IR::Type_Extern *type);
-    // const IR::Node *preorder(IR::Declaration_MatchKind *decl);
-    // const IR::Node *preorder(IR::Type_Method *type);
-    ///////////////////////////////////////////////////////////////////////
-    const IR::Node *preorder(IR::Parameter *param) override { return param; }
-    const IR::Node *preorder(IR::NamedExpression *ne) override { return ne; }
-    const IR::Node *preorder(IR::TypeParameters *p) override {
-        prune();
-        return p;
-    }
-    const IR::Node *preorder(IR::Declaration_Variable *decl) override;
-    const IR::Node *preorder(IR::Declaration *decl) override {
-        return process(decl);
-    }
-    const IR::Node *preorder(IR::Type_Declaration *decl) override {
-        return process(decl);
-    }
+    const IR::Node *preorder(IR::Type_StructLike *type) override;
+    const IR::Node *preorder(IR::Type_Extern *type) override;
+    const IR::Node *preorder(IR::Type_Method *type) override;
+    const IR::Node *
+    PruneUnused::preorder(IR::Declaration_Variable *decl) override;
 };
 
 class PruneAllUnused : public PassManager {
  public:
     explicit PruneAllUnused(P4::ReferenceMap *refMap) {
-
         passes.emplace_back(new PassRepeated{new P4::ResolveReferences(refMap),
                                              new PruneUnused(refMap)});
     }
