71abf7928221ac1e2157cdf8ee42fddf168c6c0a
Anish <aa8143@nyu.edu>
2021-03-05 20:16:21 +0530

    actions don't like to be on root

--- do_install.sh
+++ do_install.sh
@@ -7,9 +7,9 @@ SRC_DIR="$(pwd)"
 
 # fetch submodules and update apt
 echo "Pulling in gauntlet"
-git clone https://github.com/p4gauntlet/gauntlet /gauntlet
-rm -rf /gauntlet/modules/pruner
-ln -s ${SRC_DIR} /gauntlet/modules/pruner
-cd /gauntlet 
+git clone https://github.com/p4gauntlet/gauntlet
+rm -rf gauntlet/modules/pruner
+ln -s ${SRC_DIR} gauntlet/modules/pruner
+cd gauntlet 
 ./do_install.sh
 
